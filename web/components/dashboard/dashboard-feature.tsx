25527afdd4c4e68b1039baf2e231a7f7c8ac27be
👻 <y@uklok.ai>
2024-06-14 04:27:15 -0400

    feat(web): 👻 Play Button
    - A play button component was created to be used for landing game access.
    - Initial implementation.
    - Randomness & UX implemented.
    - Tailwind Nesting plugin activated.
    - Dashboard modified to include the button for testing.

--- web/components/dashboard/dashboard-feature.tsx
+++ web/components/dashboard/dashboard-feature.tsx
@@ -1,37 +1,17 @@
 'use client';
 
-import { AppHero } from '../ui/ui-layout';
-
-const links: { label: string; href: string }[] = [
-  { label: 'Solana Docs', href: 'https://docs.solana.com/' },
-  { label: 'Solana Faucet', href: 'https://faucet.solana.com/' },
-  { label: 'Solana Cookbook', href: 'https://solanacookbook.com/' },
-  { label: 'Solana Stack Overflow', href: 'https://solana.stackexchange.com/' },
-  {
-    label: 'Solana Developers GitHub',
-    href: 'https://github.com/solana-developers/',
-  },
-];
+import { PlayButton } from '@/components/shared';
 
 export default function DashboardFeature() {
   return (
-    <div>
-      <AppHero title="gm" subtitle="Say hi to your new Solana dApp." />
-      <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
-        <div className="space-y-2">
-          <p>Here are some helpful links to get you started.</p>
-          {links.map((link, index) => (
-            <div key={index}>
-              <a
-                href={link.href}
-                className="link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link.label}
-              </a>
-            </div>
-          ))}
+    <div className="hero min-h-full bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+            Wanna try your luck? Click the button below to get started.
+          </p>
+          <PlayButton />
         </div>
       </div>
     </div>
