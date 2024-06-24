fcc8f0fc966f3c34bef1f9f1f90677399079e667
👻 <y@uklok.ai>
2024-06-24 19:39:06 -0400

    feat(web): 👻 Lucky Wallet Provider
    - The `LuckyWalletAdapter` is being injected into the Solana WalletProvider.
    - The `LuckyBags` context is being loaded.
    - The `SolanaProvider` directly implements the `LuckyWalletProvider`.
    - Crypto and LuckyBags providers were implemented in the Root Layout.

--- web/providers/index.ts
+++ web/providers/index.ts
@@ -1,4 +1,7 @@
 export * from './react-query-provider';
+export * from './crypto-provider';
+export * from './lucky-bags-provider';
+export * from './wallet-provider';
 export * from './solana-provider';
 
 export * from '@/components/cluster/cluster-data-access';
