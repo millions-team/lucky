d938774b86f05e019e240de1357ba80d5d3e23e5
👻 <y@uklok.ai>
2024-06-29 19:11:50 -0400

    test(anchor): 👻 Bounty | Games Program
    - All the bounty features and changes were properly adjusted and tested.

--- anchor/tests/bounty.spec.ts
+++ anchor/tests/bounty.spec.ts
@@ -2,10 +2,11 @@ import * as anchor from '@coral-xyz/anchor';
 import { BN } from '@coral-xyz/anchor';
 import { Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 import {
-  createAssociatedTokenAccount,
+  Account,
   createMint,
-  getAccount,
+  getOrCreateAssociatedTokenAccount,
   mintToChecked,
+  getAccount,
 } from '@solana/spl-token';
 
 import {
@@ -13,11 +14,12 @@ import {
   getGamePDA,
   getGameModePDA,
   encodeName,
-  getKeeperPDA,
-  getStrongholdPDA,
+  getTreasurePDA,
   getBountyPDA,
+  getEscrowPDA,
   getBountyVaultPDA,
   toBN,
+  TREASURE_FORGE_COST,
 } from '../src/games-exports';
 
 const DECIMALS = 8;
@@ -38,6 +40,25 @@ describe('Bounty', () => {
   let gem: PublicKey; // Mint of token to be used for bounties.
   let trader: PublicKey; // Mint of token to be charged to players for playing the game.
   let accounts: Record<string, PublicKey>;
+
+  const mintTo = (receiver: PublicKey, amount: BN, mint: PublicKey) =>
+    mintToChecked(
+      connection, // connection
+      supplier, // fee payer
+      mint, // mint
+      receiver, // receiver (must be a token account)
+      supplier, // mint authority
+      amount, // amount. if your decimals is 8, you mint 10^8 for 1 token.
+      DECIMALS
+    );
+
+  const getTokenAccount = (owner: PublicKey, mint: PublicKey) =>
+    getOrCreateAssociatedTokenAccount(
+      connection, // connection
+      supplier, // fee payer
+      mint, // mint
+      owner // owner,
+    );
 
   beforeAll(async () => {
     const payer = supplier;
@@ -75,69 +96,30 @@ describe('Bounty', () => {
   });
 
   describe('Preparing the Stronghold', () => {
+    const payer = Keypair.generate();
+
     beforeAll(async () => {
-      const payer = supplier;
-
-      const reserve = await createAssociatedTokenAccount(
-        connection, // connection
-        payer, // fee payer
-        gem, // mint
-        payer.publicKey // owner,
+      const tx = await connection.requestAirdrop(
+        payer.publicKey,
+        (TREASURE_FORGE_COST + 0.1) * LAMPORTS_PER_SOL
       );
-
-      // const bag = await createAssociatedTokenAccount(
-      //   connection, // connection
-      //   payer, // fee payer
-      //   trader, // mint
-      //   payer.publicKey // owner,
-      // );
-
-      await Promise.all(
-        [
-          [gem, toBN(100_000_000, DECIMALS), reserve],
-          // [trader, toBN(100, DECIMALS), bag],
-        ].map(([mint, amount, receiver]) =>
-          mintToChecked(
-            connection, // connection
-            payer, // fee payer
-            mint, // mint
-            receiver, // receiver (must be a token account)
-            payer, // mint authority
-            amount, // amount. if your decimals is 8, you mint 10^8 for 1 token.
-            DECIMALS
-          )
-        )
-      );
-
-      accounts['reserve'] = reserve;
-      // accounts['bag'] = bag;
+      await connection.confirmTransaction(tx);
     });
 
-    it('Should forge and stockpile the stronghold for the gem', async () => {
-      const { reserve: account } = accounts;
+    it('Should forge a stronghold for the gem', async () => {
+      const { gem } = accounts;
+      const treasure = await program.account.treasure.fetchNullable(
+        getTreasurePDA()
+      );
 
+      const timeout = new Promise((resolve) =>
+        setTimeout(resolve, treasure ? 0 : 1000)
+      );
+      await timeout;
       await program.methods
         .forgeStronghold()
-        .accounts(accounts)
-        .signers([supplier])
-        .rpc();
-
-      const reserve = await getAccount(connection, account);
-      await program.methods
-        .stockpileGems(new BN(reserve.amount.toString()))
-        .accounts(accounts)
-        .signers([supplier])
-        .rpc();
-    });
-
-    it('Should forge a stronghold for the trader', async () => {
-      const { trader: gem } = accounts;
-
-      // Forge a stronghold to test failure test cases.
-      await program.methods
-        .forgeStronghold()
-        .accounts({ gem, supplier: supplier.publicKey })
-        .signers([supplier])
+        .accounts({ gem, supplier: payer.publicKey })
+        .signers([payer])
         .rpc();
     });
   });
@@ -171,74 +153,114 @@ describe('Bounty', () => {
   });
 
   describe('Issue', () => {
-    it('Should initialize a bounty', async () => {
-      const { gem, task, trader } = accounts;
-      const settings = {
-        reward: toBN(1000, DECIMALS),
-        price: toBN(0.5, DECIMALS),
-      };
+    describe('Safeguarded gem', () => {
+      it('Should initialize a bounty', async () => {
+        const { gem, task, trader } = accounts;
+        const settings = {
+          reward: toBN(1000, DECIMALS),
+          price: toBN(0.5, DECIMALS),
+        };
 
-      await program.methods
-        .issueBounty(settings)
-        .accounts(accounts)
-        .signers([supplier])
-        .rpc();
+        await program.methods
+          .issueBounty(settings)
+          .accounts(accounts)
+          .signers([supplier])
+          .rpc();
 
-      const bountyPDA = getBountyPDA(task, gem, trader);
-      const bounty = await program.account.bounty.fetch(bountyPDA);
+        const bountyPDA = getBountyPDA(task, gem, trader);
+        const bounty = await program.account.bounty.fetch(bountyPDA);
 
-      expect(bounty.owner).toEqual(supplier.publicKey);
-      expect(bounty.task).toEqual(task);
+        expect(bounty.owner).toEqual(supplier.publicKey);
+        expect(bounty.task).toEqual(task);
 
-      expect(bounty.gem).toEqual(gem);
-      expect(bounty.reward.toString()).toEqual(settings.reward.toString());
+        expect(bounty.gem).toEqual(gem);
+        expect(bounty.reward.toString()).toEqual(settings.reward.toString());
 
-      expect(bounty.price.toString()).toEqual(settings.price.toString());
-      expect(bounty.trader).toEqual(trader);
+        expect(bounty.price.toString()).toEqual(settings.price.toString());
+        expect(bounty.trader).toEqual(trader);
 
-      expect(bounty.currentlyIssued.toString()).toEqual('0');
-      expect(bounty.winners).toEqual(0);
-      expect(bounty.totalClaimed.toString()).toEqual('0');
+        expect(bounty.currentlyIssued.toString()).toEqual('0');
+        expect(bounty.winners).toEqual(0);
+        expect(bounty.totalClaimed.toString()).toEqual('0');
+      });
+    });
+
+    describe('Unknown gem', () => {
+      let gem: PublicKey;
+
+      beforeAll(async () => {
+        gem = await createMint(
+          connection, // conneciton
+          supplier, // fee payer
+          supplier.publicKey, // mint authority
+          null, // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
+          DECIMALS
+        );
+      });
+
+      it('Should fail to initialize a bounty', async () => {
+        const { task, trader } = accounts;
+        const settings = {
+          reward: toBN(1000, DECIMALS),
+          price: toBN(0.5, DECIMALS),
+        };
+
+        await expect(
+          program.methods
+            .issueBounty(settings)
+            .accounts({ task, trader, supplier: supplier.publicKey, gem })
+            .signers([supplier])
+            .rpc()
+        ).rejects.toThrow(/stronghold(.+)AccountNotInitialized/);
+      });
     });
   });
 
   describe('Fund', () => {
+    let reserve: Account;
+
+    beforeAll(async () => {
+      const reserve = await getTokenAccount(supplier.publicKey, gem);
+      await mintTo(reserve.address, toBN(100_000_000, DECIMALS), gem);
+
+      accounts['reserve'] = reserve.address;
+      // accounts['bag'] = bag;
+    });
+
+    beforeEach(async () => {
+      reserve = await getTokenAccount(supplier.publicKey, gem);
+    });
+
     it(`Should fail to init and incorrect gem`, async () => {
       const { bounty: bountyPDA, trader } = accounts;
-      const strongholdPDA = getStrongholdPDA(trader);
-      const stronghold = await getAccount(connection, strongholdPDA);
+      const reserve = await getTokenAccount(supplier.publicKey, trader);
 
       const bounty = await program.account.bounty.fetch(bountyPDA);
       const amount = BigInt(bounty.reward * 10);
-      expect(stronghold.amount.toString()).toEqual('0');
 
       await expect(
         program.methods
           .fundBounty(new BN(amount.toString()))
           .accounts({
+            supplier: supplier.publicKey,
+            reserve: reserve.address,
             bounty: bountyPDA,
             gem: trader,
-            supplier: supplier.publicKey,
           })
           .signers([supplier])
           .rpc()
       ).rejects.toThrow(/InvalidGem/);
     });
 
-    it(`Should init bounty vault and transport the gems from stronghold`, async () => {
+    it(`Should init bounty vault and transport the gems from reserve`, async () => {
       const { bounty: bountyPDA, vault: vaultPDA } = accounts;
-      const strongholdPDA = getStrongholdPDA(gem);
-      const strongholdBeforeTransport = await getAccount(
-        connection,
-        strongholdPDA
-      );
       const bountyBeforeTransport = await program.account.bounty.fetch(
         bountyPDA
       );
 
       const amount = BigInt(bountyBeforeTransport.reward * 10);
       expect(bountyBeforeTransport.currentlyIssued.toString()).toEqual('0');
-      expect(strongholdBeforeTransport.amount).toBeGreaterThanOrEqual(amount);
+      expect(reserve.amount).toBeGreaterThanOrEqual(amount);
 
       await program.methods
         .fundBounty(new BN(amount))
@@ -246,14 +268,15 @@ describe('Bounty', () => {
         .signers([supplier])
         .rpc();
 
-      const stronghold = await getAccount(connection, strongholdPDA);
+      const reserveAfterTransport = await getAccount(
+        connection,
+        reserve.address
+      );
       const bounty = await program.account.bounty.fetch(bountyPDA);
       const vault = await getAccount(connection, vaultPDA);
 
-      expect(vault.owner).toEqual(getKeeperPDA());
-      expect(stronghold.amount).toEqual(
-        strongholdBeforeTransport.amount - amount
-      );
+      expect(vault.owner).toEqual(getEscrowPDA());
+      expect(reserveAfterTransport.amount).toEqual(reserve.amount - amount);
       expect(vault.amount.toString()).toEqual(amount.toString());
       expect(bounty.currentlyIssued.toString()).toEqual(
         vault.amount.toString()
@@ -262,12 +285,10 @@ describe('Bounty', () => {
 
     it('Should fail to fund bounty with uncollectible amount', async () => {
       const { bounty: bountyPDA } = accounts;
-      const strongholdPDA = getStrongholdPDA(gem);
-      const stronghold = await getAccount(connection, strongholdPDA);
 
       const bounty = await program.account.bounty.fetch(bountyPDA);
       const amount = BigInt(bounty.reward * 10.5); // Not a multiple of the bounty reward.
-      expect(stronghold.amount).toBeGreaterThanOrEqual(amount);
+      expect(reserve.amount).toBeGreaterThanOrEqual(amount);
 
       await expect(
         program.methods
@@ -280,18 +301,16 @@ describe('Bounty', () => {
 
     it(`Should fail to fund and incorrect gem`, async () => {
       const { bounty: bountyPDA, trader } = accounts;
-      const strongholdPDA = getStrongholdPDA(trader);
-      const stronghold = await getAccount(connection, strongholdPDA);
-
+      const reserve = await getTokenAccount(supplier.publicKey, trader);
       const bounty = await program.account.bounty.fetch(bountyPDA);
       const amount = BigInt(bounty.reward * 10);
-      expect(stronghold.amount.toString()).toEqual('0');
 
       await expect(
         program.methods
           .fundBounty(new BN(amount.toString()))
           .accounts({
             bounty: bountyPDA,
+            reserve: reserve.address,
             gem: trader,
             supplier: supplier.publicKey,
           })
