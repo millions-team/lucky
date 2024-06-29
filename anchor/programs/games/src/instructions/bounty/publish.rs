b88b6ec69b351740369769fb9604c23efc4687f0
👻 <y@uklok.ai>
2024-06-29 18:53:58 -0400

    feat(anchor): 👻 Publish | Bounty
    - Bounties can only be issued with gem already safeguarded by a treasure stronghold.

--- anchor/programs/games/src/instructions/bounty/publish.rs
+++ anchor/programs/games/src/instructions/bounty/publish.rs
@@ -1,7 +1,7 @@
 pub use crate::state::{game_mode::GameMode, bounty::{Bounty, BountySettings}};
-use crate::constants::{BOUNTY_SEED};
+use crate::constants::{BOUNTY_SEED, VAULT_SEED};
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint};
+use anchor_spl::token::{Mint, TokenAccount};
 
 pub fn new_bounty(bounty: &mut Bounty, settings: BountySettings) -> Result<()> {
     bounty.price = settings.price.clone();
@@ -27,6 +27,14 @@ pub struct InitializeBounty<'info> {
     pub task: Account<'info, GameMode>,
     pub gem: Account<'info, Mint>,
     pub trader: Account<'info, Mint>,
+
+    // To prevent a bounty from an unknown gem.
+    #[account(
+        seeds = [VAULT_SEED, gem.key().as_ref()],
+        token::mint = gem,
+        bump
+    )]
+    stronghold: Account<'info, TokenAccount>,
 
     system_program: Program<'info, System>,
 }
